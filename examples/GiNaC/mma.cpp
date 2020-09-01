750e265c2e7957b16b0e488ac674ea18dd882801
Feng Feng <F.Feng@outlook.com>
2020-09-01 14:24:50 +0800

    add mma.cpp in GiNaC

--- examples/GiNaC/mma.cpp
+++ examples/GiNaC/mma.cpp
@@ -18,8 +18,13 @@ int main() {
     cout << "collect w.r.t. {a,b}:" << endl;
     cout << mma_collect(ex_num, lst{a,b}, true, true) << endl;
     cout << "check: 0 = " << normal(num-ex_num.subs(lst{coCF(w)==w,coVF(w)==w})) << endl;
-
+    cout << endl;
     
+    cout << nd << endl;
+    auto ex_s3 = mma_series(nd,s,3);
+    cout << "series up to O(s^3):" << endl;
+    cout << ex_s3 << endl;
+    cout << endl;
     
 
 
